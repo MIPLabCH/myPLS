b69c557bd31b2df7aefca383bba8a2dfa7a94ff0
valkebets <30644256+valkebets@users.noreply.github.com>
2019-09-25 17:18:45 +0800

    Update myPLS_cov.m
    Updated header, minor formatting changes

--- myPLS_functions/myPLS_cov.m
+++ myPLS_functions/myPLS_cov.m
@@ -1,40 +1,41 @@
 function R = myPLS_cov(X,Y,grouping,grouped_PLS)
 
-% Compute PLS cross-covariance matrix (stacked)
-
-% IN:
-%   X            : N x M matrix, N is #subjects, M is #imaging variables
-%   Y            : N x B matrix, B is #behaviors
-%   grouping     : N x 1 vector, subject grouping for PLS analysis
-%                     e.g. [1,1,2] = subjects 1&2 belong to group 1,
-%                     subject 3 belongs to group 2.
-%   grouped_PLS  : binary variable indicating if groups should be 
-%                     considered when computing R
+% This function generates a cross-covariance matrix (stacked) for the PLS
+% analysis
+% 
+% Inputs:
+% - X            : N x M matrix, N is #subjects, M is #imaging variables
+% - Y            : N x B matrix, B is #behaviors
+% - grouping     : N x 1 vector, subject grouping (e.g. diagnosis)
+%                  e.g. [1,1,2] = subjects 1 and 2 belong to group 1,
+%                  subject 3 belongs to group 2
+% - grouped_PLS  : binary variable indicating if groups should be 
+%                  considered when computing R
 %              0 = PLS will computed over all subjects (ignoring grouping)
 %              1 = R will be constructed by concatenating group-wise
-%                  covariance matrices ( as in conventional behavior PLS)
+%                  covariance matrices (as in conventional behavior PLS)
 %
-% OUT:
-%   R : cross-covariance matrix
+% Outputs:
+% - R            : cross-covariance matrix
 %
 %
-% function based on myPLS_norm by Dimitri Van De Ville
-%   modifications D. Zoeller (Aug 2019): 
-%       - adapted to work also for different group labels than 1,2,3,... 
-%         (e.g. 2,3,4)
-%       - removed group number input (can be derived from grouping vector)
+% Function based on myPLS_cov by Dimitri Van De Ville
+% Modifications by D. Zoeller (Aug 2019): 
+%   - adapted to work also for different group labels than 1,2,3,... (e.g. 2,3,4)
+%   - removed group number input (can be derived from grouping vector)
+
 
 if ~grouped_PLS
-    grouping=ones(size(grouping));
+    grouping = ones(size(grouping));
 end
 
-% number and IDs of groups
-groupIDs=unique(grouping);
-nGroups=length(groupIDs);
+% Number and IDs of groups
+groupIDs = unique(grouping);
+nGroups = length(groupIDs);
 
-for iG=1:nGroups
-    Ysel = Y(grouping==groupIDs(iG),:);
-    Xsel = X(grouping==groupIDs(iG),:);
+for iG = 1:nGroups
+    Ysel = Y(grouping == groupIDs(iG),:);
+    Xsel = X(grouping == groupIDs(iG),:);
     
     R0 = Ysel.'*Xsel;
 
@@ -44,4 +45,3 @@ for iG=1:nGroups
         R = [R; R0];
     end
 end
-
