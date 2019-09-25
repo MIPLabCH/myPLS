c17af61fc9332c296494aa349f2d616ee2a3b16b
valkebets <30644256+valkebets@users.noreply.github.com>
2019-09-25 17:22:41 +0800

    Update myPLS_get_boot_orders.m
    Updated header, minor formatting changes

--- myPLS_functions/myPLS_get_boot_orders.m
+++ myPLS_functions/myPLS_get_boot_orders.m
@@ -1,34 +1,44 @@
 function all_boot_orders = myPLS_get_boot_orders(nBootstraps,grouping,grouped_boot)
 
-% function to compute bootstrapping sampling orders for bootstrapping with
-% resampling
-% if groups are considered for resampling, groups can be not ordered
+% This function computes bootstrapping sampling orders for bootstrapping 
+% with resampling
+% Note that if groups are considered for resampling, groups can be not ordered
+%
+% Inputs:
+% - nBootstraps     : P, #number of bootstrap samples
+% - grouping        : N x 1 vector, subject grouping (e.g., diagnosis)
+% - grouped_boot    : binary variable indicating if groups should be 
+%                     considered during bootstrapping
+%                     0 = bootstrapping ignoring grouping
+%                     1 = bootstrapping within group
+%
+% Outputs:
+% - all_boot_orders : N x P, subject index in each bootstrap sample
 
 
-% ignoring grouping information if asked for
+% Ignore grouping information if asked for
 if ~grouped_boot
-    grouping=ones(size(grouping));
+    grouping = ones(size(grouping));
 end
 
+% Get number of groups and their indices
+groupIDs = unique(grouping);
+nGroups = length(groupIDs);
 
-% get number of groups and their indices
-groupIDs=unique(grouping);
-nGroups=length(groupIDs);
-
-% number of subjects
+% Number of subjects
 nSubj = length(grouping); 
 
 % Get bootstrap subject sampling
 all_boot_orders = nan(nSubj,nBootstraps);
-for iG = 1:nGroups
-    % getting the IDs of the subjects in this group
-    groupID_find=find(grouping == groupIDs(iG));
-    num_subj_group = length(groupID_find);
 
-    % compute bootstrapping orders for the number of subjects in this group
+for iG = 1:nGroups
+    % Get the IDs of the subjects in this group
+    groupID = find(grouping == groupIDs(iG));
+    num_subj_group = length(groupID);
+
+    % Compute bootstrapping orders for the number of subjects in this group
     [boot_order_tmp,~] = rri_boot_order(num_subj_group,1,nBootstraps);
 
-    % change the order of the group indices according to the bootstrapping
-    all_boot_orders(groupID_find,:) = groupID_find(boot_order_tmp);
+    % Change the order of the group indices according to the bootstrapping
+    all_boot_orders(groupID,:) = groupID(boot_order_tmp);
 end
-
