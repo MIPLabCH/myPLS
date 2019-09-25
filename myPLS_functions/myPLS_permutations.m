8a1a1196f28c3f2fd819b09500fca5c3246c0967
valkebets <30644256+valkebets@users.noreply.github.com>
2019-09-25 17:24:43 +0800

    Update myPLS_permutations.m
    Updated header, minor formatting changes

--- myPLS_functions/myPLS_permutations.m
+++ myPLS_functions/myPLS_permutations.m
@@ -1,18 +1,18 @@
 function Sp_vect = myPLS_permutations(X,Y,U,grouping,pls_opts)
-%
-% Permutation testing over singular values obtained with PLS
-% Rows (subjects) of Y are permuted within each diagnostic group
+
+% This function computed permutation testing over singular values obtained with PLS
+% Rows (subjects) of Y are permuted within each group
 %
 % Inputs:
 % - X          : N x M matrix, N is #subjects, M is #imaging
 % - Y          : N x B matrix, B is #behaviors
 % - U          : B x L matrix, L is #latent components, behavior saliences
-% - grouping   : N x 1 vector, subject (diagnostic) grouping 
-%                   e.g. [1,1,2] = subjects 1&2 belong to group 1,
-%                   subject 3 belongs to group 2.
-% - pls_opts : options for the PLS analysis
+% - grouping   : N x 1 vector, subject grouping (e.g. (diagnosis)
+%                e.g. [1,1,2] = subjects 1 and 2 belong to group 1,
+%                subject 3 belongs to group 2
+% - pls_opts   : options for the PLS analysis
 %                necessary fields for this function:
-%       - .nPerms : number of permutations
+%       - .nPerms       : number of permutations
 %       - .grouped_perm : binary variable indicating if groups should be 
 %               considered during the permutations
 %              0 = permutations ignoring grouping
@@ -23,10 +23,9 @@ function Sp_vect = myPLS_permutations(X,Y,U,grouping,pls_opts)
 %              1 = R will be constructed by concatenating group-wise
 %                  covariance matrices ( as in conventional behavior PLS)
 %
-%
 % Outputs:
-% - Sp_vect             : matrix with bootstrapping salience values, from
-%                         these, the p-values can be computed
+% - Sp_vect    : matrix with bootstrapping salience values, used to compute
+%                p-values                         
 
 
 % Check that dimensions of X & Y are correct
@@ -35,21 +34,20 @@ if(size(X,1) ~= size(Y,1))
 end
 
 disp('... Permutations ...')
-for iter_perm = 1:pls_opts.nPerms
+for iP = 1:pls_opts.nPerms
     
     % Display number of permutations (every 50 samples) - (Dani: my personal
     % preference is to include a skipped line once in a while)
-    if mod(iter_perm,20) == 0, fprintf('%d ',iter_perm); end
-    if ~mod(iter_perm,200); fprintf('\n'); end
+    if mod(iP,20) == 0, fprintf('%d ',iP); end
+    if ~mod(iP,200); fprintf('\n'); end
     
     % Leave X unchanged (no need to permute both X and Y matrices)
     Xp = X; % X is already normalized
     
-    % Permute Y (within each diagnostic group, or across all subjects,
-    % according to setup)
+    % Permute Y (within each group or across all subjects)
     Yp = myPLS_permuteY(Y,grouping,pls_opts.grouped_perm);
     
-    % Cross-covariance matrix between X and permuted Y
+    % Generate cross-covariance matrix between X and permuted Y
     Rp = myPLS_cov(Xp,Yp,grouping,pls_opts.grouped_PLS);
     
     % Singular value decomposition of Rp
@@ -61,8 +59,8 @@ for iter_perm = 1:pls_opts.nPerms
     Sp = sqrt(sum(Up.^2));
     
     % Keep singular values for sample distribution of singular values
-    Sp_vect(:,iter_perm) = Sp';    
+    Sp_vect(:,iP) = Sp';    
 end
 
-if mod(iter_perm,200); fprintf('\n'); end
+if mod(iP,200); fprintf('\n'); end
 disp(' ')
